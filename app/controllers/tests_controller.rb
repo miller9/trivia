1abb5a63cf28abba17d72cd122ecb5519bb1cae3
miller <millerpoli9@gmail.com>
2018-12-14 21:02:43 -0500

    Adding -create action- on the tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -5,5 +5,18 @@ class TestsController < ApplicationController
 
 	def new
 	end
-	
+
+	def create
+		@test = Test.new(test_params)
+		@test.save
+
+		redirect_to @test
+	end
+
+	private
+
+	def test_params
+		params.require(:test).permit(:question, :answer)
+	end
+
 end
