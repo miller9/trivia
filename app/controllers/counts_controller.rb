b3ce472a28c91ee16026c96701572728676a9aea
miller <millerpoli9@gmail.com>
2018-12-16 07:22:18 -0500

    -routes -> adding all routes for new counts_controller,    - counts_controller -> adding random & show functions and their views-

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -1,2 +1,15 @@
 class CountsController < ApplicationController
+
+	def index
+	end
+
+	def random
+		@ticket = Random.new.rand(100_000..999_999) 
+		# puts @ticket
+	end
+
+	def show
+		@ticket = random
+	end
+
 end
