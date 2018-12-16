43b3b822f69a9ae0ce191537e8d078de0f4de5dc
miller <millerpoli9@gmail.com>
2018-12-16 11:52:33 -0500

    DELETING the -Count Model- to reverse changes and let appon a stable phase --> start working on another branches

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_160305) do
-
-  create_table "counts", force: :cascade do |t|
-    t.string "nickname"
-    t.integer "points"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_12_16_014454) do
 
   create_table "tests", force: :cascade do |t|
     t.datetime "created_at", null: false
